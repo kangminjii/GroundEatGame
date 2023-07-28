20f7b450a4d8d2c025084342ca9aa16c333d3da5
rkdalswl789 <rkdalswl789@gmail.com>
2023-07-28 18:16:51 +0900

    0728

--- GroundEatGame/GroundEatGame.cpp
+++ GroundEatGame/GroundEatGame.cpp
@@ -8,7 +8,58 @@
 #include <vector>
 
 #define MAX_LOADSTRING 100
+
 using namespace std;
+
+// >> : BITMAP
+#pragma comment(lib, "msimg32.lib")
+
+HBITMAP hBackImage;
+BITMAP  bitBack;
+
+HBITMAP hFrontImage;
+BITMAP  bitFront;
+
+void CreateBitmap();
+void DeleteBitmap();
+// << : BITMAP
+
+// >> : Double Buffering
+HBITMAP hDoubleBufferImage;
+RECT rectView;
+
+void DrawBitmapDoubleBuffering(HWND hWnd, HDC hdc);
+// << : Double Buffering
+
+
+// 키보드 입력
+enum { Left, Right, Up, Down, Painting, Moving, None };
+static int state = None;
+static int paintState = None;
+static int previousState = None;
+
+//// 주인공
+// 위치 및 반지름
+static POINT center = { 300, 200 };
+int radius = 10;
+// 지나간 길
+static vector<POINT> location;
+static bool moveLR = TRUE;  // 지나간 선 위에 있을 때 좌우 통제
+static bool moveUD = TRUE;  // 지나간 선 위에 있을 때 상하 통제
+
+//// 배경
+// 색칠
+static vector<POINT> painted;    // 색칠할 지점, 계속 초기화하고 갱신할 변수
+static vector<POINT> startRange; // startRange = 꺾인 점
+static vector<POINT> endRange;   // endRange   = 그려진 선분과 만나는 점 
+static int endRangeCount = 0; // endRange index 증가용
+
+static vector<int> paintSize;  // 그릴 도형의 꼭짓점 개수 저장 벡터
+static int paintSizeCount = 0; // 그릴 도형의 꼭짓점 개수
+static bool endSaved = FALSE;
+
+POINT* temp = new POINT;      // 색칠된 도형 저장
+static vector<POINT*> shapes; // temp를 저장할 벡터
 
 
 #ifdef UNICODE
@@ -66,7 +117,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     return (int) msg.wParam;
 }
-
 
 
 //
@@ -137,53 +187,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     PAINTSTRUCT ps;
     HDC hdc;
-    HBRUSH hBrush, oldBrush;
-
-    // 화면 경계선
-    static RECT rectView;
-
-    // 키보드 입력
-    enum{Left, Right, Up, Down, Painting, Moving, None};
-    static int state = None;
-    static int paintState = None;
-    static int previousState = None;
-    
-    //// 주인공
-    // 위치
-    static POINT center = { 100, 100 };
-    int radius = 10;
-    // 지나간 길
-    static vector<POINT> location;
-    
-    //// 색칠
-    // 색칠할 지점, 계속 초기화하고 갱신할 변수
-    static vector<POINT> painted;
-    static bool isFull = FALSE;
-    // startRange = 꺾인 점, endRange = 그려진 선분과 만나는 점
-    static vector<POINT> startRange;
-    static vector<POINT> endRange;
-    static int count = 0; // endRange index 증가용
-    static int paintSize = 0;
-    static int tempSize = 0;
-
-    // 색칠된 도형 저장
-    static vector<POINT*> shapes;
 
     switch (message)
     {
-
     case WM_CREATE: // 초기화 값 세팅
     {
-        SetTimer(hWnd, 1, 500, NULL);
-
+        //SetTimer(hWnd, 1, 100, NULL);
         location.push_back(center);
-        //painted.push_back(center);
+
+        GetClientRect(hWnd, &rectView);
+        CreateBitmap();
     }
     break;
 
     case WM_TIMER:
-    {
-        InvalidateRect(hWnd, NULL, TRUE);
+    {   
+        //InvalidateRect(hWnd, NULL, TRUE);
     }
     break;
 
@@ -202,18 +221,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             paintState = None;
 
         // 그리기 상태
-        if (paintState == Painting && isFull == FALSE)
+        if (paintState == Painting)
         {
             if (GetAsyncKeyState(VK_LEFT) & 0x8000)
             {
-                if (previousState != Left)
+                if (previousState != Left && previousState != Right)
                 {
                     painted.push_back(center);
                     startRange.push_back(center);
+                    paintSizeCount++;
                 }
                 else
                 {
-                    if (state != Right)
+                    if (state != Right && moveLR == TRUE)
                     {
                         center.x -= 5;
                         state = Left;
@@ -223,14 +243,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
             {
-                if (previousState != Right)
+                if (previousState != Right && previousState != Left)
                 {
                     painted.push_back(center);
                     startRange.push_back(center);
+                    paintSizeCount++;
                 }
                 else
                 {
-                    if (state != Left)
+                    if (state != Left && moveLR == TRUE)
                     {
                         center.x += 5;
                         state = Right;
@@ -240,14 +261,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             else if (GetAsyncKeyState(VK_UP) & 0x8000)
             {
-                if (previousState != Up)
+                if (previousState != Up && previousState != Down)
                 {
                     painted.push_back(center);
                     startRange.push_back(center);
+                    paintSizeCount++;
                 }
                 else
                 {
-                    if (state != Down)
+                    if (state != Down && moveUD == TRUE)
                     {
                         center.y -= 5;
                         state = Up;
@@ -257,14 +279,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             else if (GetAsyncKeyState(VK_DOWN) & 0x8000)
             {
-                if (previousState != Down)
+                if (previousState != Down && previousState != Up)
                 {
                     painted.push_back(center);
                     startRange.push_back(center);
+                    paintSizeCount++;
                 }
                 else
                 {
-                    if (state != Up)
+                    if (state != Up && moveUD == TRUE)
                     {
                         center.y += 5;
                         state = Down;
@@ -274,75 +297,114 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             else
                 break;
-
+            
             location.push_back(center);
         }
         // 테두리에서 이동할수 있는 상태
         else if(paintState == Moving)
         {
+            endSaved = FALSE;
+
             if (GetAsyncKeyState(VK_LEFT) & 0x8000)
             {
-                center.x -= 5;
-                state = Left;
+                if (moveLR == TRUE)
+                {
+                    for (int i = 1; i < startRange.size(); i++)
+                    {
+                        if (startRange[i - 1].x <= center.x && center.x <= startRange[i].x)
+                            center.x -= 5;
+                    }
+                    state = Left;
+                }
             }
             else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
             {
-                center.x += 5;
-                state = Right;
+                if (moveLR == TRUE)
+                {
+                    for (int i = 1; i < startRange.size(); i++)
+                    {
+                        if (startRange[i - 1].x <= center.x && center.x <= startRange[i].x)
+                            center.x += 5;
+                    }
+                    state = Right;
+                }
             }
             else if (GetAsyncKeyState(VK_UP) & 0x8000)
             {
-                center.y -= 5;
-                state = Up;
+                if (moveUD == TRUE)
+                {
+                    for (int i = 1; i < startRange.size(); i++)
+                    {
+                        if (startRange[i - 1].y <= center.y && center.y <= startRange[i].y)
+                            center.y -= 5;
+                    }
+                    state = Up;
+                }
             }
             else if (GetAsyncKeyState(VK_DOWN) & 0x8000)
             {
-                center.y += 5;
-                state = Down;
+                if (moveUD == TRUE)
+                {
+                    for (int i = 1; i < startRange.size(); i++)
+                    {
+                        if (startRange[i - 1].y <= center.y && center.y <= startRange[i].y)
+                            center.y += 5;
+                    }
+                    state = Down;
+                }
+                
             }
         }
-       
-        // endRange 설정
+
+        // 선분 위에 있음을 체크
         if (startRange.size() > 1)
         {
             for (int i = 1; i < startRange.size(); i++)
             {
-                if (startRange[i-1].x == startRange[i].x && center.x == startRange[i].x) // 일직선에 있을 때
+                if (startRange[i - 1].x == startRange[i].x && center.x == startRange[i].x) // 일직선에 있을 때(가로)
+                {
+                    if (startRange[i - 1].y < center.y && center.y < startRange[i].y)
+                        moveLR = FALSE;
+                    else
+                        moveLR = TRUE;
+                }
+                if (startRange[i - 1].y == startRange[i].y && center.y == startRange[i].y) // 일직선에 있을 때(세로)
+                {
+                    if (startRange[i - 1].x < center.x && center.x < startRange[i].x)
+                        moveUD = FALSE;
+                    else
+                        moveUD = TRUE;
+                }
+            }
+        }
+
+        // endRange 설정
+        if (startRange.size() > 1 && paintState == Painting)
+        {
+            for (int i = 1; i < startRange.size(); i++)
+            {
+                if (startRange[i - 1].x == startRange[i].x && center.x == startRange[i].x) // 일직선에 있을 때(가로)
                 {
                     if (startRange[i - 1].y < center.y && center.y < startRange[i].y)
                     {
                         endRange.push_back(center);
-                        paintSize = startRange.size();
+                        endSaved = TRUE;
+                        break;
                     }
                 }
-                if (startRange[i - 1].y == startRange[i].y && center.y == startRange[i].y) // 일직선에 있을 때
+                if (startRange[i - 1].y == startRange[i].y && center.y == startRange[i].y) // 일직선에 있을 때(세로)
                 {
                     if (startRange[i - 1].x < center.x && center.x < startRange[i].x)
                     {
                         endRange.push_back(center);
-                        paintSize = startRange.size();
+                        endSaved = TRUE;
+                        break;
                     }
                 }
             }
         }
 
-        /////////////@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        // 내부로 안들어가게끔 설정
-        if (startRange.size() > 1)
-        {
-            for (int i = 1; i < startRange.size(); i++)
-            {
-                double distanceLine = sqrt((startRange[i - 1].x - startRange[i].x) * (startRange[i - 1].x - startRange[i].x)
-                                         + (startRange[i - 1].y - startRange[i].y) * (startRange[i - 1].y - startRange[i].y));
-
-            }
-        }
-
-
-
-
-
-        InvalidateRect(hWnd, NULL, TRUE);
+        InvalidateRect(hWnd, NULL, FALSE);
     }
     break;
     case WM_COMMAND:
@@ -367,39 +429,63 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
         hdc = BeginPaint(hWnd, &ps);
 
-        // 선그리기 시작점
-        MoveToEx(hdc, location.front().x, location.front().y, NULL);
+        // 뒷배경 & 더블버퍼링
+        DrawBitmapDoubleBuffering(hWnd, hdc);
 
         // 그리기 상태
-        int size = painted.size() + 1;
-        POINT* temp = new POINT[size];
-        
         if (paintState == Painting)
         {
-            // 조작에 따른 선 그리기
-            for (int i = 0; i < location.size(); i++)
-                LineTo(hdc, location[i].x, location[i].y);
-            
-            // 영역 표시하기
-            if (endRange.size() == count + 1) // endRange.size()가 1이여야 첫 색칠이 시작됨
-            {
-                cout << "영역 표시하기 count :" << count << endl;
-                temp[0] = endRange[count];
-                // 영역 색칠하기
-                for (int m = 1; m < painted.size(); m++)
-                {
-                    temp[m] = painted[m];
-                }
-                if(size != 1)
-                    temp[size - 1] = endRange[count++];
+            // 선그리기 시작점
+            MoveToEx(hdc, location.front().x, location.front().y, NULL);
 
-               /* cout << "Start" << endl;
-                for (int i = 0; i < painted.size(); i++)
+            // 조작에 따른 선 그리기
+            for (int i = 1; i < location.size(); i++)
+            {
+                LineTo(hdc, location[i].x, location[i].y);
+            }
+
+            if (endSaved == TRUE)
+            {
+                // 영역 표시하기
+                if (endRange.size() == endRangeCount + 1) // endRange.size()가 1이여야 첫 색칠이 시작됨
                 {
-                    cout << "temp : " << temp[i].x << ", " << temp[i].y << endl;
-                    cout << "painted : " << painted[i].x << ", " << painted[i].y << endl;
-                }*/
-                shapes.push_back(temp);
+                    cout << "색칠 되는 도형의 수 : " << endRangeCount << endl;
+                    //cout << "paint size count : " << paintSizeCount << endl;
+
+                    // 영역 색칠하기
+                    for (int m = 0; m < painted.size(); m++)
+                    {
+                        if (endRangeCount == 0 && m == 0)
+                            temp[0] = endRange[endRangeCount];
+                        else
+                            temp[m] = painted[m];
+
+                    }
+                    temp[painted.size()] = endRange[endRangeCount++];
+
+                    shapes.push_back(temp);
+
+                    cout << "Start" << endl;
+                    for (int i = 0; i < paintSizeCount; i++)
+                    {
+                        //cout << "temp : " << temp[i].x << ", " << temp[i].y << endl;
+                        //cout << "painted : " << painted[i].x << ", " << painted[i].y << endl;
+                    }
+
+                    // 색칠된 부분이면 선이 표시되지 않게끔 설정
+                    location.clear();
+                    painted.clear();
+
+                    // 현재 위치를 시작점으로 설정
+                    location.push_back({ center.x, center.y });
+                    painted.push_back({ center.x, center.y });
+
+                    // 도형의 꼭짓점 개수 파악
+                    paintSize.push_back(paintSizeCount);
+                    paintSizeCount = 0;
+
+                    endSaved = FALSE;
+                }
             }
         }
         // 테두리에서 이동할수 있는 상태
@@ -408,20 +494,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             for (int i = 0; i < location.size(); i++)
                 LineTo(hdc, location[i].x, location[i].y);
         }
-
-
-        hBrush = CreateSolidBrush(RGB(0, 0, 255));
-        oldBrush = (HBRUSH)SelectObject(hdc, hBrush);
-
-        if (paintSize > 0)
-        {
-            for (int i = 0; i < shapes.size(); i++)
-                Polygon(hdc, shapes[i], paintSize);
-        }
-
-        SelectObject(hdc, oldBrush);
-        DeleteObject(hBrush);
-        
         // 플레이어
         Ellipse(hdc, center.x - radius, center.y - radius, center.x + radius, center.y + radius);
         
@@ -430,9 +502,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     break;
 
     case WM_DESTROY:
-        //painted.clear();
-        //vector<POINT>().swap(painted);
-        KillTimer(hWnd, 1);
+        //KillTimer(hWnd, 1);
+        delete[] temp;
+        DeleteBitmap();
         PostQuitMessage(0);
     break;
 
@@ -460,4 +532,95 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+}
+
+void CreateBitmap()
+{
+    // >> : 수지
+    hBackImage = (HBITMAP)LoadImage(NULL, TEXT("images/수지.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+
+    if (hBackImage == NULL) // 이미지가 출력되지 않을 때
+    {
+        DWORD dwError = GetLastError();
+        MessageBox(NULL, _T("이미지 로드 에러1"), _T("에러"), MB_OK);
+        return;
+    }
+
+    GetObject(hBackImage, sizeof(BITMAP), &bitBack);
+
+    // >> : background
+    hFrontImage = (HBITMAP)LoadImage(NULL, TEXT("images/Background.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+
+    if (hFrontImage == NULL) // 이미지가 출력되지 않을 때
+    {
+        DWORD dwError = GetLastError();
+        MessageBox(NULL, _T("이미지 로드 에러4"), _T("에러"), MB_OK);
+        return;
+    }
+
+    GetObject(hFrontImage, sizeof(BITMAP), &bitFront);
+}
+
+void DrawBitmapDoubleBuffering(HWND hWnd, HDC hdc)
+{
+    HDC hMemDC, hMemDC2;
+    HBITMAP hOldBitmap, hOldBitmap2;
+    int bx, by;
+
+    hMemDC = CreateCompatibleDC(hdc);
+    if (hDoubleBufferImage == NULL)
+    {
+        hDoubleBufferImage = CreateCompatibleBitmap(hdc, rectView.right, rectView.bottom);
+    }
+    hOldBitmap = (HBITMAP)SelectObject(hMemDC, hDoubleBufferImage); //  기본은 검정색
+
+    // >> : 수지
+    {
+        hMemDC2 = CreateCompatibleDC(hMemDC); // 같은 포맷
+        hOldBitmap2 = (HBITMAP)SelectObject(hMemDC2, hBackImage);
+        bx = bitBack.bmWidth; // 전체 너비
+        by = bitBack.bmHeight; // 전체 높이
+
+        BitBlt(hMemDC, 0, 0, bx, by, hMemDC2, 0, 0, SRCCOPY); // 그림 그리기
+
+        SelectObject(hMemDC2, hOldBitmap2);
+        DeleteDC(hMemDC2);
+    }
+
+    // >> : background
+    {
+        hMemDC2 = CreateCompatibleDC(hMemDC); // 같은 포맷
+        hOldBitmap2 = (HBITMAP)SelectObject(hMemDC2, hFrontImage);
+        bx = bitFront.bmWidth; // 전체 너비
+        by = bitFront.bmHeight; // 전체 높이
+
+        HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255));
+        HBRUSH oldBrush = (HBRUSH)SelectObject(hMemDC2, hBrush);
+
+        // 색칠될 부분
+        if (paintSize.size() > 0)
+        {
+            for (int i = 0; i < shapes.size(); i++)
+                Polygon(hMemDC2, shapes[i], paintSize[i]);
+        }
+
+        SelectObject(hMemDC2, oldBrush);
+        DeleteObject(hBrush);
+
+        TransparentBlt(hMemDC, 0, 0, bx, by, hMemDC2, 0, 0, bx, by, RGB(255, 0, 255));
+
+        SelectObject(hMemDC2, hOldBitmap2);
+        DeleteDC(hMemDC2);
+    }
+
+    // >> : hdc에 그려주기
+    TransparentBlt(hdc, 0, 0, rectView.right, rectView.bottom, hMemDC, 0, 0, rectView.right, rectView.bottom, RGB(255, 0, 255));
+
+    SelectObject(hMemDC, hOldBitmap);
+    DeleteDC(hMemDC);
+}
+
+void DeleteBitmap()
+{
+    DeleteObject(hBackImage);
 }
